c6274bb3b17ba5d12b713b847ef15db53bb86450
Luis de Bethencourt <luisbg@amazon.com>
2018-09-13 11:55:04 +0100

    Cryptopals: 2.1

--- src/set2.rs
+++ src/set2.rs
@@ -1,5 +1,26 @@
+fn add_padding(content: String, block_len: usize) -> String {
+    let mut out = content.clone();
+
+    let rem = content.len() % block_len;
+    if rem != 0 {
+        let pad_size = block_len - rem;
+
+        for _ in 0..pad_size {
+            // out.push('\x04' as char);
+            out.push('-' as char);
+        }
+    }
+
+    out
+}
+
 fn exercise_1() {
     println!("Cryptopals: 2.1");
+    println!("Implement PKCS#7 padding");
+
+    let content = String::from("YELLOW SUBMARINE");
+    println!("Original content is: {}", content);
+    println!("Padded output is: {}", add_padding(content, 20));
 }
 
 pub fn run(exercise_num: usize) {
